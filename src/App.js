21d0a796659df867d3cfbbd73a624abde1be7823
76do <isshi0210@gmail.com>
2023-03-01 19:02:22 +0900

    Add: チャット一覧ページへのルーティングとコンポーネントを作成した。

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {Header} from './containers/Header.jsx';
 import {LoginHeader} from './containers/LoginHeader.jsx';
 import {Footer} from './containers/Footer.jsx';
 import {NotReady} from './containers/NotReady.jsx';
+import {Chats} from './containers/Chats.jsx';
 import {styled, ThemeProvider, createTheme} from '@mui/system';
 import {useCookies} from 'react-cookie';
 
@@ -65,6 +66,9 @@ function App() {
 	  						/>
 	  						<Route exact path="/mypage">
 	  							<MyPage loginNotice={false}/>
+	  						</Route>
+	  						<Route exact path="/chats">
+	  							<Chats/>
 	  						</Route>
 	  						<Route path="/notready">
 	  							<NotReady/>
---
+++ src/containers/Chats.jsx
@@ -0,0 +1,48 @@
+import React, {Fragment, useState, useContext, useLayoutEffect} from 'react';
+import {styled, ThemeProvider, createTheme} from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { getUserRequests } from '../apis/GetRequests';
+import { getCurrentUser } from '../apis/GetCurrentUserInfo';
+import Alert from '@mui/material/Alert';
+import { UserInfoContext } from '../providers/UserInfoProvider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { FixedSizeList } from 'react-window';
+import MarkAsUnreadOutlinedIcon from '@mui/icons-material/MarkAsUnreadOutlined';
+import { RequestHistoryDialog } from './RequestHistoryDialog';
+import { useCookies } from 'react-cookie';
+import Fade from '@mui/material/Fade';
+import CircularProgress from '@mui/material/CircularProgress';
+import{
+	useLocation
+} from "react-router-dom";
+import {usePageTracking} from '../functions/useTracking';
+
+export const Chats = () => {
+	usePageTracking();
+	const Theme = createTheme({
+		palette: {
+			text: {
+				primary: '#263238',
+			},
+			main: {
+				primary: '#ffa500'
+			},	
+			primary: {
+				main: '#ffa500'
+			}
+		},
+	});
+
+	return(
+		<Fragment>
+			<ThemeProvider theme={Theme}>
+			<Container maxWidth='lg'>
+			</Container>
+			</ThemeProvider>
+		</Fragment>
+	)
+}
