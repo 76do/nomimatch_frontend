43ef2088c5fdd45e5fc59479878dae38c309f8b1
76do <isshi0210@gmail.com>
2023-03-01 19:51:15 +0900

    Add: チャットカード及び一覧作成

--- src/containers/Chats.jsx
+++ src/containers/Chats.jsx
@@ -7,9 +7,7 @@ import { getUserRequests } from '../apis/GetRequests';
 import { getCurrentUser } from '../apis/GetCurrentUserInfo';
 import Alert from '@mui/material/Alert';
 import { UserInfoContext } from '../providers/UserInfoProvider';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList } from 'react-window';
 import MarkAsUnreadOutlinedIcon from '@mui/icons-material/MarkAsUnreadOutlined';
 import { RequestHistoryDialog } from './RequestHistoryDialog';
@@ -20,6 +18,14 @@ import{
 	useLocation
 } from "react-router-dom";
 import {usePageTracking} from '../functions/useTracking';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import ChatIcon from '@mui/icons-material/Chat';
 
 export const Chats = () => {
 	usePageTracking();
@@ -37,10 +43,44 @@ export const Chats = () => {
 		},
 	});
 
+	function renderRow(props) {
+  	const { index, style } = props;
+
+  	return (
+    	<ListItem style={style} key={index} component="div" disablePadding >
+      		<ListItemButton>
+			  <ListItem alignItems="flex-start">
+				<ListItemAvatar>
+					<ChatIcon
+						sx={{fontSize: 30, color: 'main.primary', pt: 1, pb: 1}}
+					/ >
+				</ListItemAvatar>
+				<ListItemText
+				  primary="たあさん"
+				  secondary={
+					<React.Fragment>
+					  {" 飲み会依頼 ........."}
+					</React.Fragment>
+				  }
+				/>
+			  </ListItem>
+      		</ListItemButton>
+    	</ListItem>
+  	);
+	}
 	return(
 		<Fragment>
 			<ThemeProvider theme={Theme}>
 			<Container maxWidth='lg'>
+				<FixedSizeList
+   				height={400}
+   				width={360}
+   				itemSize={80}
+   				itemCount={100}
+   				overscanCount={5}
+   				>
+   				{renderRow}
+      			</FixedSizeList>
 			</Container>
 			</ThemeProvider>
 		</Fragment>
